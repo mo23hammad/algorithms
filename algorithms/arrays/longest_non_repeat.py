28881b21ed87c2ba7c3d900ca21ece1630d77de2
Mohammad Mahdi Abedi <143641806+mo23hammad@users.noreply.github.com>
2024-05-07 22:09:08 +0400

    Update longest_non_repeat.py
    adding "p" without "p" it is not correct

--- algorithms/arrays/longest_non_repeat.py
+++ algorithms/arrays/longest_non_repeat.py
@@ -5,7 +5,7 @@ without repeating characters.
 Examples:
 Given "abcabcbb", the answer is "abc", which the length is 3.
 Given "bbbbb", the answer is "b", with the length of 1.
-Given "pwwkew", the answer is "wke", with the length of 3.
+Given "pwwkew", the answer is "pwke", with the length of 3.
 Note that the answer must be a substring,
 "pwke" is a subsequence and not a substring.
 """
